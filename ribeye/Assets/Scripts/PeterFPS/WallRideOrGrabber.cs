7b6e43909f4e66dbd95f085e2c100af6162aa996
peter <peterlimail47@gmail.com>
2021-12-03 21:52:07 -0800

    stashing

--- ribeye/Assets/Scripts/PeterFPS/WallRideOrGrabber.cs
+++ ribeye/Assets/Scripts/PeterFPS/WallRideOrGrabber.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.Experimental.GraphView;
-using UnityEditor.UI;
 using UnityEngine;
 
 public class WallRideOrGrabber : MonoBehaviour
