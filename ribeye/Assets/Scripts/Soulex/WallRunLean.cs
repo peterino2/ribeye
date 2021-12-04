fdf2cc842ba78eeca0d2945bb12d6888e039565a
Fiber Soulz <fnafiplayar@gmail.com>
2021-12-04 00:51:59 -0800

    fixed brokey wallrun

--- ribeye/Assets/Scripts/Soulex/WallRunLean.cs
+++ ribeye/Assets/Scripts/Soulex/WallRunLean.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 
 public class WallRunLean : MonoBehaviour
 {
-    [SerializeField] private Transform cam;
-    [SerializeField] private Transform player;
+    public Transform cam;
+    //[SerializeField] private Transform player;
     [SerializeField] private AnimationCurve animCurve;
-    public IEnumerator Lean()
+    public IEnumerator Lean(float dir)
     {
         float pos = 0;
-        float ogYpos = player.position.y;
+        Vector3 ogPos = cam.localPosition;
         while (pos < 1)
         {
-            pos += Time.deltaTime * 2.5f;
-            cam.rotation = Quaternion.Euler(new Vector3(0, 0, animCurve.Evaluate(pos)));
-            player.position = new Vector3(player.position.x, ogYpos + animCurve.Evaluate(pos), player.position.z);
+            pos += Time.deltaTime * 0.6f;
+            cam.localRotation = Quaternion.Euler(new Vector3(0, 0, animCurve.Evaluate(pos) * 10 * dir));
+            cam.localPosition = new Vector3(0, ogPos.y + animCurve.Evaluate(pos), 0);
             yield return null;
         }
+        StartCoroutine(ResetLean());
+    }
+    public IEnumerator ResetLean()
+    {
+        float w = 0;
+        while (w < 1)
+        {
+            w += Time.deltaTime;
+            cam.localPosition = Vector3.Lerp(cam.localPosition, Vector3.zero, Time.deltaTime * 10);
+            cam.localRotation = Quaternion.Lerp(cam.localRotation, Quaternion.identity, Time.deltaTime * 10);
+            yield return null;
+        }
+        cam.localPosition = Vector3.zero;
+        cam.localRotation = cam.localRotation;
     }
 }
