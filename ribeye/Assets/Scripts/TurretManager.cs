d6d7b95f1d1aa283c5f0dfa764dd8add0e7a88e3
ZaCkOX <zacko1000@hotmail.com>
2021-12-05 15:39:08 -0800

    Revert "Moving around AI Turret"
    This reverts commit 603059eb04b10ac5c869278b975c1a7c93c291e1.

--- ribeye/Assets/Scripts/TurretManager.cs
+++ ribeye/Assets/Scripts/TurretManager.cs
@@ -11,6 +11,36 @@ public class TurretManager : MonoBehaviour {
     [Header("Setup")]
     [SerializeField] private LookAtAndLocker[] lookAtAndLockerScripts = default;
 
+    //Declare publics
+    [NonSerialized] public Transform DummyTransform = default;
+
+    #endregion
+
+    #region Constants
+
+    //Declare private constants
+    private const string strDUMMY_TRANSFORM = "DummyTransform";
+
+    #endregion
+
+    #region Start and initialization
+
+    private void Start() {
+        //Create dummy transform
+        CreateDummyTransform();
+    }
+
+    private void CreateDummyTransform() {
+        //Create dummy transform
+        GameObject objDummy = new GameObject();
+        //Create dummy transform
+        DummyTransform = objDummy.transform;
+        //Change name
+        DummyTransform.name = strDUMMY_TRANSFORM;
+        //Disable
+        DummyTransform.gameObject.SetActive(false);
+    }
+
     #endregion
 
     #region Update
