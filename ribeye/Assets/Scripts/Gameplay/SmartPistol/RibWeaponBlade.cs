ce0712d9c64118db1144fe76a3953bfa798fadb8
peter <peterlimail47@gmail.com>
2021-12-07 03:15:23 -0800

    added sword hurt box

--- ribeye/Assets/Scripts/Gameplay/SmartPistol/RibWeaponBlade.cs
+++ ribeye/Assets/Scripts/Gameplay/SmartPistol/RibWeaponBlade.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using Game;
+using Gameplay.Stats;
 using UnityEngine;
 
 namespace Gameplay.Gunner
 {
     public class RibWeaponBlade : RibWeaponBase
     {
+        public float damage = 5;
         private Collider hurtbox;
+        private bool activated = false;
+        [SerializeField] private GameObject impactEffect;
         private void Awake()
         {
             activationIndex = 1;
@@ -19,6 +25,7 @@ namespace Gameplay.Gunner
         [SerializeField] private GameObject model;
         public override void ActivateWeapon()
         {
+            activated = true;
             model.SetActive(true);
             gunAnimator.Play("BladeEquip");
         }
@@ -33,9 +40,81 @@ namespace Gameplay.Gunner
             gunAnimator.Play("Armature|HookToss");
         }
 
+        [SerializeField]
+        private List<EntityBase> targets = new List<EntityBase>();
+
+        public LayerMask playermask;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            print("nigga");
+            if (other.gameObject.GetComponent<EntityBase>()
+                && ((( 1 << other.gameObject.layer) | playermask.value) > 0)
+            )
+            {
+                targets.Add(other.gameObject.GetComponent<EntityBase>());
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            var e = other.GetComponent<EntityBase>();
+            if (e == null)
+            {
+                targets.Remove(e);
+            }
+            if (targets.Contains(e))
+            {
+                targets.Remove(e);
+            }
+        }
+
+        private float swingDamageDelay = 0;
+        private void Update()
+        {
+            if(swingDamageDelay > 0)
+            {
+                swingDamageDelay -= Time.deltaTime;
+                if (swingDamageDelay < 0)
+                {
+                    foreach (var target in targets)
+                    {
+                        if (target != null)
+                        {
+                            target.TakeDamage(damage);
+                            Physics.Raycast(
+                                gunner.transform.position,
+                                target.transform.position - gunner.transform.position,
+                                out RaycastHit r, Mathf.Infinity, ~playermask);
+                            
+                            Instantiate(impactEffect, r.point, Quaternion.LookRotation(r.normal));
+                        }
+                    }
+                }
+            }
+            
+
+            if (cooldown > 0)
+            {
+                cooldown -= Time.deltaTime;
+                if (cooldown < 0)
+                {
+                    swingReady = true;
+                }
+            }
+        }
+
+        private bool swingReady = true;
+        private float cooldown = 0f;
         public override void OnFire()
         {
-            gunAnimator.Play("BladeSwingSeq1");
+            if (swingReady)
+            {
+                swingReady = false;
+                gunAnimator.Play("BladeSwingSeq1");
+                swingDamageDelay = 0.1f;
+                cooldown = 0.3f;
+            }
         }
         
         public override void OnReloadPressed()
@@ -44,6 +123,7 @@ namespace Gameplay.Gunner
 
         public override void DeactivateWeaponNoAnim()
         {
+            activated = false;
             model.SetActive(false);
         }
         
@@ -56,5 +136,7 @@ namespace Gameplay.Gunner
         {
             return "Blade and tackle";
         }
+        
+        
     }
 }
