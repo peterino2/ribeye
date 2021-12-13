e64b24f0a0d24fcc22e4b15394e10044f2454ad5
peter <peterlimail47@gmail.com>
2021-12-12 21:44:34 -0800

    started connecting the core

--- ribeye/Assets/MainMenuPlayButton.cs
+++ ribeye/Assets/MainMenuPlayButton.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuPlayButton : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<Button>().onClick.AddListener(TaskOnClick);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene("LevelOne");
     }
 }
